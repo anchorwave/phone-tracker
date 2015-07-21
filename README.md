e2ff81e15f6aa643e7cc8a0638a9db1b5cc59ef9
Jameel Bokhari <me@jameelbokhari.com>
2015-07-21 15:26:24 -0700

    fixed update, does not clash with 1.1.10

--- README.md
+++ README.md
@@ -11,6 +11,17 @@ Pushing updates through github may fail without the following:
 Optional:
 + Update `$config['requires']` to required version of WordPress
 
-**This is for for GitHub Updater**
+*This is for for GitHub Updater*
 
-~Current Version:1.0.6~
+~Current Version:1.0.8~
+
+#### changelog
+
+**v1.0.8**
+: Fixed WP_Github_Updater clash, now compatible with phone-number-swappy 1.1.10
+
+**v1.0.7**
+: Fixed incorrect index error in `twilio.php`
+
+**v1.0.0 .. 6**
+: Added update functionallity... various fixes throughout.
--- twillo.php
+++ twillo.php
@@ -3,7 +3,7 @@
 Plugin Name: WP Phone Tracker
 Plugin URI: https://www.anchorwave.com
 Description: This plugin is used to track phone numbers on twillo.com
-Version: 1.0.6
+Version: 1.0.8
 Author: Anchor Wave
 Author URI: https://www.anchorwave.com
 License: GPL2
@@ -11,13 +11,19 @@ License: GPL2
 
 
 define('MY_BASE', plugin_dir_path( __FILE__ ));
-require_once MY_BASE . 'update.php';
+
 add_action('admin_init', 'phone_tracker_updates');
+
 function phone_tracker_updates(){
+	if ( ! class_exists( 'WP_GitHub_Updater' ) )
+		require_once MY_BASE . 'update.php';
+
 	if ( is_admin() && class_exists('WP_GitHub_Updater') && WP_GitHub_Updater::VERSION == '1.6' ) {
+
 		if ( !defined('WP_GITHUB_FORCE_UPDATE') && isset( $_GET['force-check'] ) && $_GET['force-check'] == '1' && current_user_can('update_plugins') ){	
 			define('WP_GITHUB_FORCE_UPDATE', true);
 		}
+
 	    $config = array(
 	        'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
 	        'proper_folder_name' => 'phone-tracker', // this is the name of the folder your plugin lives in
@@ -32,12 +38,16 @@ function phone_tracker_updates(){
 	        'access_token' => '', // Access private repositories by authorizing under Appearance > GitHub Updates when this example plugin is installed
 	    );
 	    $githubupdater = new WP_GitHub_Updater($config);
+
 	}
+
 }
 
 require_once(MY_BASE . 'config/config.php');
+
 register_activation_hook(__FILE__, array('tw_install', 'install'));
 register_deactivation_hook( __FILE__, array('tw_install', 'uninstall') );
+
 
 function tw_phone_numbers()
 {
@@ -69,14 +79,14 @@ function tw_calls()
 	$tw_cont_obj->calls();
 }
 
-if(is_numeric($_GET['tw_phone_call']) && $_GET['tw_phone_call'] > 0)
+if( isset( $_GET['tw_phone_call'] ) && is_numeric( $_GET['tw_phone_call'] ) && $_GET['tw_phone_call'] > 0)
 {
 	$p_id =(int) $_GET['tw_phone_call'];
 	$tw_cont_obj->called($p_id);
 	exit;
 }
 
-if(is_numeric($_GET['tw_phone_record']) && $_GET['tw_phone_record'] > 0)
+if( isset( $_GET['tw_phone_record'] ) && is_numeric( $_GET['tw_phone_record'] ) && $_GET['tw_phone_record'] > 0)
 {
 	$callId =(int) $_GET['tw_phone_record'];
 	$tw_cont_obj->recording($callId);
